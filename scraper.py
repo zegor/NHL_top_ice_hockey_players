a0fb16cef2abb13e3088114f67764e6fb0511b3a
zegor <greg.solon@gmail.com>
2018-07-14 21:27:30 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -46,6 +46,16 @@ for group in range(6):
     for link in player_row_link:
       player_link_text = link[2]
     
+    # Scrape player source
+    html_player = scraperwiki.scrape(player_link_text)
+    
+    # Parse HTML
+    root_player = lxml.html.fromstring(html_player)
+    
+    player_bio = root_player.cssselect("div.player-bio")
+    player_metadata = player_bio.cssselect("ul.player-metadata.floatleft")
+    print(player_metadata.text_content())
+    
     # Write out to the sqlite database using scraperwiki library
     scraperwiki.sqlite.save(unique_keys=["Player ID"], data={"Player ID":player_id, "Category":category_name_text, "Player Rank":player_rank_text, "Player Name":player_name_text, "Player Link":player_link_text}, table_name="data")
     player_id += 1
