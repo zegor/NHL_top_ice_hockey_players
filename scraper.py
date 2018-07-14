082c8b9c3fd35ebd27d3d20486935884980671dc
zegor <greg.solon@gmail.com>
2018-07-14 18:58:41 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,7 +22,7 @@ groupings = root.cssselect("table.tablehead")
 
 for group in range(6):
   categories = groupings[group].cssselect("tbody")
-  print(categories)
+  print(categories[group].text_content())
   
   # for category in categories:
     # category_name = category.cssselect("td")
